ad386300915e7abcf9b230e4a529000d43c2938d
JoaoBizarro <joao1bizarro@gmail.com>
2017-02-01 21:25:25 +0000

    Finds references in the text

--- ProjetoDAS/src/export/ExportHtml.java
+++ ProjetoDAS/src/export/ExportHtml.java
@@ -2,11 +2,26 @@ package export;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ExportHtml extends Export{
 
     @Override
     public void exportText(String text, String path) {
+        ArrayList<String> refer = new ArrayList<>();
+        int n = 1;
+        Pattern p = Pattern.compile("\\\\cite\\{(.*?)\\}");
+        Matcher m = p.matcher(text);
+        while (m.find()) {
+            if(!refer.contains(m.group(1))){
+                text = text.replace("\\cite{" + m.group(1) + "}", "[" + n + "]");
+                refer.add(m.group(1));
+                n++;
+            }
+        }
+        
         FileWriter fWriter = null;
         BufferedWriter writer = null;
         try {
